60a864235710d273f93201dd676628a059a8fcbf
Manda mora <shiestybizz113@gmail.com>
2026-08-15 19:54:45 -0700

    add economic truth boundary: backend/economic_truth/__init__.py

---
+++ backend/economic_truth/__init__.py
@@ -0,0 +1,11 @@
+from .domain import ActionState, EconomicAction, EconomicTruthError, verify_receipt
+from .registry import CoverageRegistry, EconomicSurface
+
+__all__ = [
+    "ActionState",
+    "EconomicAction",
+    "EconomicTruthError",
+    "verify_receipt",
+    "CoverageRegistry",
+    "EconomicSurface",
+]
